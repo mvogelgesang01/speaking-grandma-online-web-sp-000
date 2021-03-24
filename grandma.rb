5b460542b784ad6bcf43b267e11eb459c29aaf12
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-24 20:17:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma= "I LOVE YOU GRANDMA!"
+def speak_to_grandma
 end
 
 if speak_to_grandma == "I LOVE GRANDMA!"
