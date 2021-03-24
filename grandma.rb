9009e987aff528e9a3a03ce3828b20e3abd77084
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-24 21:57:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,9 @@ string = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else 
-    return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    return "NO, NOT SINCE 1938!"
   end
 end
